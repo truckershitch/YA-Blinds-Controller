dcc10f211221bb775dc65c40536d2d8e46520961
truckershitch <truckershitch@hambone.e4ward.com>
2022-03-03 00:50:55 -0500

    boot.py fix - was a symlink

--- boot.py
+++ boot.py
@@ -1,1 +1,14 @@
-boot.py.no_repl
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+import uos
+uos.dupterm(None, 1) # disable REPL on UART(0)
+import sys
+import gc
+import webrepl
+webrepl.start()
+gc.collect()
+del sys.path[0]
+sys.path.append('')
+from connect_wifi import connect
+connect()
